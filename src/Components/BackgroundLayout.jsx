ffe5b0b75459ea9fdf9fc2eee98da5b604c67d33
satyam <skill.improve24@gmail.com>
2024-04-07 04:48:01 +0530

    Completed Weather App

---
+++ src/Components/BackgroundLayout.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useStateContext } from "../Context";
+//images
+import Clear from "../assets/Images/Clear.jpg";
+import Fog from "../assets/Images/fog.png";
+import Cloudy from "../assets/Images/Cloudy.jpg";
+import Rainy from "../assets/Images/Rainy.jpg";
+import Snow from "../assets/Images/snow.jpg";
+import Stormy from "../assets/Images/Stormy.jpg";
+import Sunny from "../assets/Images/Sunny.jpg";
+
+const BackgroundLayout = () => {
+  const { weather } = useStateContext();
+  const [image, setImage] = useState(Clear);
+
+  useEffect(() => {
+    if (weather.conditions) {
+      let imageString = weather.conditions;
+      if (imageString.toLowerCase().includes("clear")) {
+        setImage(Clear);
+      } else if (imageString.toLowerCase().includes("cloud")) {
+        setImage(Cloudy);
+      } else if (
+        imageString.toLowerCase().includes("rain") ||
+        imageString.toLowerCase().includes("shower")
+      ) {
+        setImage(Rainy);
+      } else if (imageString.toLowerCase().includes("snow")) {
+        setImage(Snow);
+      } else if (imageString.toLowerCase().includes("fog")) {
+        setImage(Fog);
+      } else if (
+        imageString.toLowerCase().includes("thunder") ||
+        imageString.toLowerCase().includes("storm")
+      ) {
+        setImage(Stormy);
+      }
+    }
+  }, [weather]);
+
+  return (
+    <img
+      src={image}
+      alt="weather_image"
+      className="h-screen w-full fixed left-0 top-0 -z-[10]"
+    />
+  );
+};
+
+export default BackgroundLayout;
---
+++ src/Components/index.jsx
@@ -0,0 +1,5 @@
+// import BackgroundLayout from "./BackgroundLayout";
+
+export { default as BackgroundLayout } from "./BackgroundLayout";
+export { default as MiniCard } from "./MiniCard";
+export { default as WeatherCard } from "./WeatherCard";
