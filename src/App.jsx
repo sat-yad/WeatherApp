cfc41dc3a4ba94fb31e78c48f86e88302472ad8c
satyam <skill.improve24@gmail.com>
2024-04-07 16:55:04 +0530

    added Hover Effect

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,9 @@ function App() {
   return (
     <div className="w-full h-screen text-white px-8">
       <nav className="w-full p-3 flex justify-between items-center">
-        <h1 className="font-bold tracking-wide text-3xl">Weather App</h1>
+        <h1 className="font-bold tracking-wide text-3xl hover:scale-105 transition duration-300 ease-in-out hover:shadow-custom weather-app-text">
+          Weather App
+        </h1>
         <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
           <img src={search} alt="search" className="w-[1.5rem] h-[1.5rem]" />
           <input
@@ -31,7 +33,7 @@ function App() {
             }}
             type="text"
             placeholder="Search city"
-            className="focus:outline-none w-full text-[#212121] text-lg"
+            className="focus:outline-none w-full text-[#212121] text-lg "
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
