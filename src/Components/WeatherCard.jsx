451ccb8030e57e4474ea9489299c2d83d2265f47
satyam <skill.improve24@gmail.com>
2024-04-07 16:53:08 +0530

    added some Hover Effect

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -22,6 +22,10 @@ const WeatherCard = ({
   const [icon, setIcon] = useState(sun);
   const { time } = useDate();
 
+  const handleWindSpeedClick = () => {
+    setIsWindSpeedClicked(!isWindSpeedClicked);
+  };
+
   useEffect(() => {
     if (iconString) {
       if (iconString.toLowerCase().includes("cloud")) {
@@ -43,32 +47,43 @@ const WeatherCard = ({
   }, [iconString]);
 
   return (
-    <div className="w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4">
+    <div className="w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4 hover:scale-105 transition duration-300 ease-in-out hover:shadow-custom">
       <div className="flex w-full just-center, items-center gap-4 mt-12 mb-4">
         <img src={icon} alt="weather_icon" />
-        <p className="font-bold text-5xl flex justify-center items-center">
-          {temperature} &deg;C
+        <p className="font-bold text-5xl flex justify-center items-center weather-card-text ">
+          <span className="hover:text-blue-500">{temperature} &deg;C</span>
         </p>
       </div>
-      <div className="font-bold text-center text-xl">{place}</div>
+      <div className="font-bold text-center text-xl ">
+        <span className="hover:text-blue-500">{place}</span>
+      </div>
       <div className="w-full flex justify-between items-center mt-4">
-        <p className="flex-1 text-center p-2">{new Date().toDateString()}</p>
-        <p className="flex-1 text-center p-2">{time}</p>
+        <span className="flex-1 text-center p-2 hover:text-blue-500">
+          {new Date().toDateString()}
+        </span>
+        <span className="flex-1 text-center p-2 hover:text-blue-500">
+          {time}
+        </span>
+        {/* <p className="flex-1 text-center p-2 hover:text-blue-500">{time}</p> */}
       </div>
       <div className="w-full flex justify-between items-center mt-4 gap-4">
         <p className="flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg">
-          Wind Speed <p className="font-normal">{windspeed} km/h</p>
+          Wind Speed <p className="font-normal ">{windspeed} km/h</p>
         </p>
-        <p className="flex-1 text-center p-2 font-bold rounded-lg bg-green-600">
+        <p className="flex-1 text-center p-2 font-bold rounded-lg bg-orange-600">
           Humidity <p className="font-normal">{humidity} gm/m&#179;</p>
         </p>
       </div>
       <div className="w-full p-3 mt-4 flex justify-between items-center">
-        <p className="font-semibold text-lg">Heat Index</p>
-        <p className="text-lg">{heatIndex ? heatIndex : "N/A"}</p>
+        <span className="font-semibold text-lg hover:text-blue-500">
+          Heat Index
+        </span>
+        <span className="text-lg hover:text-blue-500">
+          {heatIndex ? heatIndex : "N/A"}
+        </span>
       </div>
       <hr className="bg-slate-600" />
-      <div className="w-full p-4 flex justify-center items-center text-3xl font-semibold">
+      <div className="w-full p-4 flex justify-center items-center text-3xl font-semibold hover:text-blue-500">
         {conditions}
       </div>
     </div>
